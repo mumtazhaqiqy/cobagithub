da87557e35af5e7831e1334f44c12e81be22aa83
unknown <mumtazhaqiqy@gmail.com>
2019-01-31 18:31:50 +0700

    commit #1

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <p>Hello World</p>
+    <p>Okay Htis is seccond line</p>
+    <p>this is second line</p>
+
+  </body>
+</html>
